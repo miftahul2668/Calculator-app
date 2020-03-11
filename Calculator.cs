b0968433adda510f54d87b60b728af6e35dfbcfa
miftahul2668 <miftahul.01@students.amikom.ac.id>
2020-03-11 11:33:11 +0700

    Praktikum 1

---
+++ Calculator.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CalculatorConsoleAPP
+{
+      class Program
+        {
+           static void Main(string[] arga)
+           {
+         
+           }
+        }
+
+}
+ 
